133fbe6b3a0d653df7852c8f91071316ce00752a
milkeddy1 <milkeddy1@gmail.com>
2024-10-16 18:15:11 +0800

    feat: implement video-list component

--- app/(secret)/dashboard/videos/page.tsx
+++ app/(secret)/dashboard/videos/page.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import VideoList from "@/components/video-list";
 import React from "react";
 
 export default function Videos() {
   return (
-    <div>
-      <Link href="/dashboard">
-        <Button>Home</Button>
-      </Link>
+    <div className="flex flex-col gap-4 w-full p-16">
+      <div className="flex justify-between w-full">
+        <h1 className="text-2xl font-bold mb-6">影片列表</h1>
+        <Button>重新整理</Button>
+      </div>
+
+      <div>
+        <Input className="w-[240px]" placeholder="搜尋影片" />
+      </div>
+
+      <div>
+        <VideoList />
+      </div>
     </div>
   );
 }
